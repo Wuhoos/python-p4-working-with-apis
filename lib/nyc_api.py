04ff58997ebb0910db01f3be9376776c7617ddb6
David <wjw3298@gmail.com>
2023-10-12 14:37:38 -0400

    complete lab

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -9,7 +9,21 @@ class GetPrograms:
 
     response = requests.get(URL)
     return response.content
+  
+  def program_school(self):
+    program_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+      program_list.append(program['agency'])
+
+    return program_list
 
 
-programs = GetPrograms().get_programs()
-print(programs)
+# programs = GetPrograms().get_programs()
+# print(programs)
+
+programs = GetPrograms()
+programs_schools = programs.program_school()
+
+for school in set(programs_schools):
+    print(school)
